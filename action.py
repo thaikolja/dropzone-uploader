7a2e7231d5f15687b975791f8f1208382a34a124
Kolja Nolte <kolja.nolte@gmail.com>
2024-09-03 23:15:14 +0700

    FTP_TLS -> SFTP Storage Box
    Signed-off-by: Kolja Nolte <kolja.nolte@gmail.com>

--- action.py
+++ action.py
@@ -1,6 +1,6 @@
 # Dropzone Action Info
 # Name:                 yanawa.io Dropzone Uploader
-# Description:          Lädt eine Datei mit Zufallsnamen auf https://yanawa.io/temp/ hoch.
+# Description:          Lädt eine Datei mit Zufallsnamen auf https://uploads.yanawa.io/ hoch.
 # Handles:              Files
 # Creator:              Kolja Nolte
 # URL:                  https://www.kolja-nolte.com
@@ -15,8 +15,8 @@
 # Import the dotenvfile module, which provides a way to load environment variables from a .env file
 import dotenvfile
 
-# Import the ftplib module, which provides a way to interact with FTP servers
-import ftplib
+# Import the paramiko module for SFTP
+import paramiko
 
 # Import the os module, which provides a way to interact with the operating system
 import os
@@ -32,20 +32,20 @@ env = dotenvfile.loadfile('.env')
 
 # Define a dictionary called info that stores configuration information
 info = {
-    # The URL of the FTP server
-    'url': env.get('HTTP_URL', 'https://host.yanawa.io'),
+    # The URL of the SFTP server
+    'url': env.get('HTTP_URL'),
 
-    # The hostname of the FTP server
-    'host': env.get('FTP_HOST', 'host.yanawa.io'),
+    # The hostname of the SFTP server
+    'host': env.get('FTP_HOST'),
 
-    # The port number of the FTP server
-    'port': env.get('FTP_PORT', 21),
+    # The port number of the SFTP server
+    'port': env.get('FTP_PORT', 22),
 
-    # The username to use for FTP authentication
-    'username': env.get('FTP_USERNAME', 'yanawa'),
+    # The username to use for SFTP authentication
+    'username': env.get('FTP_USERNAME'),
 
-    # The password to use for FTP authentication
-    'password': env.get('FTP_PASSWORD', 'password')
+    # The password to use for SFTP authentication
+    'password': env.get('FTP_PASSWORD')
 }
 
 
@@ -78,47 +78,54 @@ def dragged():
     # Initialize a list to store the names of the uploaded files
     uploaded_files_names = []
 
-    # Establish an FTP connection to the server using the ftplib.FTP_TLS class
-    with ftplib.FTP_TLS(info['host']) as ftp:
-        # Log in to the FTP server using the username and password from the info dictionary
-        ftp.login(info['username'], info['password'])
+    # Establish an SFTP connection to the server using paramiko
+    transport = paramiko.Transport((info['host'], int(info['port'])))
+    transport.connect(username=info['username'], password=info['password'])
 
-        # Iterate over the list of files to be uploaded
-        for file in files:
-            # Check if the file exists using the verify function, and skip it if it doesn't
-            if not verify(file):
-                continue
+    sftp = paramiko.SFTPClient.from_transport(transport)
 
-            # Increment the uploaded files counter
-            uploaded_files += 1
+    # Define the remote directory path
+    remote_directory = '/mnt/storage/backup/uploads/'
 
-            # Update the progress bar by setting the percent property to the current progress
-            dz.percent(int(100 * uploaded_files / files_count))
+    # Iterate over the list of files to be uploaded
+    for file in files:
+        # Check if the file exists using the verify function, and skip it if it doesn't
+        if not verify(file):
+            continue
 
-            # Get the filename and file extension from the file path
-            file_name = os.path.basename(file)
-            _, file_extension = file_name.rsplit('.', 1)
+        # Increment the uploaded files counter
+        uploaded_files += 1
 
-            # Generate a random number and convert it to a string
-            random_number = randint(1, 128).__str__()
+        # Update the progress bar by setting the percent property to the current progress
+        dz.percent(int(100 * uploaded_files / files_count))
 
-            # Generate a random name for the uploaded file using the hashlib library
-            random_name = hashlib.md5(random_number.encode()).hexdigest()[:8]
+        # Get the filename and file extension from the file path
+        file_name = os.path.basename(file)
+        _, file_extension = file_name.rsplit('.', 1)
 
-            # Create a new filename by combining the random name with the original file extension
-            new_file_name = f"{random_name}.{file_extension}"
+        # Generate a random number and convert it to a string
+        random_number = randint(1, 128).__str__()
 
-            # Open the file in binary read mode and upload it to the FTP server using the storbinary method
-            with open(file, 'rb') as file_binary:
-                ftp.storbinary(f"STOR {new_file_name}", file_binary)
-                # Add the new filename to the list of uploaded files
-                uploaded_files_names.append(new_file_name)
+        # Generate a random name for the uploaded file using the hashlib library
+        random_name = hashlib.md5(random_number.encode()).hexdigest()[:8]
 
-        # Set the percent property to 100 to indicate that the upload process is complete
-        dz.percent(100)
+        # Create a new filename by combining the random name with the original file extension
+        new_file_name = f"{random_name}.{file_extension}"
 
-        # Close the FTP connection
-        ftp.close()
+        # Print current directory as absolute
+        print(sftp.getcwd())
+
+        # Open the file in binary read mode and upload it to the specified remote directory
+        sftp.put(file, os.path.join(remote_directory, new_file_name))
+        # Add the new filename to the list of uploaded files
+        uploaded_files_names.append(new_file_name)
+
+    # Set the percent property to 100 to indicate that the upload process is complete
+    dz.percent(100)
+
+    # Close the SFTP connection
+    sftp.close()
+    transport.close()
 
     # Set the finish property of the dz object to a message indicating that the upload process is complete
     dz.finish(f"{uploaded_files} Datei(en) hochgeladen und URLs in die Zwischenablage kopiert.")
@@ -127,6 +134,6 @@ def dragged():
     # uploaded file
     if files_count == 1:
         dz.text(f"{info['url']}/{new_file_name}")
-        # If multiple files were uploaded, set the text property to a list of URLs for each file
     else:
+        # If multiple files were uploaded, set the text property to a list of URLs for each file
         dz.text("\n".join([info['url'] + '/' + file_name for file_name in uploaded_files_names]))
